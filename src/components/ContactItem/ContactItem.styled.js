c0584b36238a71d34c126affc29d1477aa1bb274
Agnieszkaa86 <agnieszka.janowska86@gmail.com>
2023-02-01 22:32:57 +0100

    Create ContactItem.styled.js

---
+++ src/components/ContactItem/ContactItem.styled.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const Button = styled.button`
+  margin: 5px 0 8px 10px;
+  font-size: 15px;
+`;
+
+export const Li = styled.li`
+  margin: 10px;
+`;
