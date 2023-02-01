c089d12c6489cd6b34c748020c19bf1b79688499
Agnieszkaa86 <agnieszka.janowska86@gmail.com>
2023-02-02 00:07:18 +0100

    Update Filter.styled.js

--- src/components/Filter/Filter.styled.js
+++ src/components/Filter/Filter.styled.js
@@ -5,3 +5,11 @@ export const Input = styled.input`
   margin-top: 15px;
   margin-bottom: 20px;
 `
+
+export const Label = styled.label`
+  display: flex;
+  flex-direction: column;
+  gap: 5px;
+  font-weight: 500;
+  font-size: 15px;
+`;
