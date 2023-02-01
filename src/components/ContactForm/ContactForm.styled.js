902aabba0ba3391483290f037de222dfae2593e1
Agnieszkaa86 <agnieszka.janowska86@gmail.com>
2023-02-01 22:32:50 +0100

    Create ContactForm.styled.js

---
+++ src/components/ContactForm/ContactForm.styled.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  max-width: 500px;
+  padding: 15px;
+  margin: 0 auto;
+`;
+export const Form = styled.form`
+  border: 1px solid;
+  padding: 15px;
+  margin: 0 auto;
+`;
+export const Input = styled.input`
+  display: block;
+  margin-top: 15px;
+  margin-bottom: 15px;
+`;
