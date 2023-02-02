82ec02cc23e60e4f28e36d0b424e3721c2ba773c
Agnieszkaa86 <agnieszka.janowska86@gmail.com>
2023-02-02 11:35:16 +0100

    Update Filter.jsx

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -2,12 +2,12 @@ import { useDispatch, useSelector } from 'react-redux';
 import { changeFilter } from '../../redux/filterSlice';
 import { getFilter } from 'redux/selectors';
 
-import { Input, Label} from './Filter.styled';
+import { Input, Label } from './Filter.styled';
 
 export const Filter = () => {
   const filter = useSelector(getFilter);
   const dispatch = useDispatch();
-   
+
   const handleChange = e => {
     dispatch(changeFilter(e.currentTarget.value));
   };
@@ -18,4 +18,4 @@ export const Filter = () => {
       <Input value={filter} type="text" onChange={handleChange} />
     </Label>
   );
-}
+};
